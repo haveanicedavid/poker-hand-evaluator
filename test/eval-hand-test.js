eb1caa59cfb710ae897f9dc0a65da05db59f8062
David Daniel <davedan7@gmail.com>
2015-09-30 00:25:40 -0600

    Basics of the final function working, but still ugly

--- test/eval-hand-test.js
+++ test/eval-hand-test.js
@@ -3,12 +3,20 @@ var evaluateHand = require('../lib/evaluate-hand.js');
 
 describe('Hand Evaluator', function() {
 
-  // it('Exists', function () {
-  //   assert(true);
-  // });
-
   it('evaluates a two of a kind', function () {
     assert.equal(evaluateHand('Ah As 4c 9h 3d'), 'Two of a Kind') ;
   });
+
+  it('evaluates a royal flush', function () {
+    assert.equal(evaluateHand('Ah Kh Qh Jh 10h'), 'Royal Flush') ;
+  });
+
+  it('evaluates a straight flush', function () {
+    assert.equal(evaluateHand('9h Kh Qh Jh 10h'), 'Straight Flush') ;
+  });
+
+  // it('evaluates a straight', function () {
+  //   assert.equal(evaluateHand('9h Kh Qh Jh 10h'), 'Straight Flush') ;
+  // });
 
 });
--- test/hand-type-check-test.js
+++ test/hand-type-check-test.js
@@ -3,7 +3,7 @@ var checkHand = require('../lib/hand-type-check.js');
 
 describe('Pair Checker', function() {
   it('Returns false if there is no pair', function () {
-    var pair = checkHand.twoOfAKind({'A': 1, 'Q': 1, 'K': 1, '9': 1, '7': 1});
+    var pair = checkHand.twoOfAKind({'14': 1, '12': 1, '13': 1, '9': 1, '7': 1});
     assert.equal(pair, false);
   });
 
@@ -109,10 +109,10 @@ describe('Flush', function() {
 
 describe('full house', function() {
   it('returns true if there is a full house', function () {
-    var fh1 = checkHand.fullHouse({'h': 3, 's': 2, 'c': 0,'d': 0});
-    var fh2 = checkHand.fullHouse({'h': 0, 's': 3, 'c': 2,'d': 0});
-    var fh3 = checkHand.fullHouse({'h': 0, 's': 0, 'c': 3,'d': 2});
-    var fh4 = checkHand.fullHouse({'h': 0, 's': 3, 'c': 0,'d': 2});
+    var fh1 = checkHand.fullHouse({'1': 3, '2': 2, '3': 0,'4': 0});
+    var fh2 = checkHand.fullHouse({'1': 0, '2': 3, '3': 2,'4': 0});
+    var fh3 = checkHand.fullHouse({'1': 0, '2': 0, '3': 3,'4': 2});
+    var fh4 = checkHand.fullHouse({'1': 0, '2': 3, '3': 0,'4': 2});
 
     assert.equal(fh1, true);
     assert.equal(fh2, true);
@@ -121,14 +121,55 @@ describe('full house', function() {
   });
 
   it('returns false if there is not a full house', function () {
-    var fh1 = checkHand.fullHouse({'h': 4, 's': 1, 'c': 0,'d': 0});
-    var fh2 = checkHand.fullHouse({'h': 1, 's': 3, 'c': 1,'d': 1});
-    var fh3 = checkHand.fullHouse({'h': 1, 's': 1, 'c': 1,'d': 2});
-    var fh4 = checkHand.fullHouse({'h': 2, 's': 1, 'c': 1,'d': 1});
+    var fh1 = checkHand.fullHouse({'1': 4, '2': 1, '3': 0,'4': 0});
+    var fh2 = checkHand.fullHouse({'1': 1, '2': 3, '3': 1,'4': 1});
+    var fh3 = checkHand.fullHouse({'1': 1, '2': 1, '3': 1,'4': 2});
+    var fh4 = checkHand.fullHouse({'1': 2, '2': 1, '3': 1,'4': 1});
 
     assert.equal(fh1, false);
     assert.equal(fh2, false);
     assert.equal(fh3, false);
     assert.equal(fh4, false);
+  });
+});
+
+describe('Straight', function() {
+  it('returns true if there is a straight', function () {
+    var s1 = checkHand.straight({1: 1, 2: 1, 3: 1, 4: 1, 5: 1});
+    var s2 = checkHand.straight({6: 1, 7: 1, 8: 1, 9: 1, 10: 1});
+
+    assert.equal(s1, true);
+    assert.equal(s2, true);
+  });
+
+  it('returns true if there is a straight regardless of order', function () {
+    var s1 = checkHand.straight({2: 1, 1: 1, 5: 1, 4: 1, 3: 1});
+    var s2 = checkHand.straight({6: 1, 7: 1, 8: 1, 9: 1, 10: 1});
+
+    assert.equal(s1, true);
+    assert.equal(s2, true);
+  });
+});
+
+describe('Straight Flush', function() {
+  it('returns true if there is a straight', function () {
+    var s1 = checkHand.straightFlush( {'h': 5, 's': 0, 'c': 0,'d': 0},{1: 1, 2: 1, 3: 1, 4: 1, 5: 1});
+    // var s2 = checkHand.straightFlush({6: 1, 7: 1, 8: 1, 9: 1, 10: 1});
+
+    // var flush1 = checkHand.flush();
+    // var flush2 = checkHand.flush({'h': 0, 's': 5, 'c': 0,'d': 0});
+    // var flush3 = checkHand.flush({'h': 0, 's': 0, 'c': 5,'d': 0});
+    // var flush4 = checkHand.flush({'h': 0, 's': 0, 'c': 0,'d': 5});
+
+    assert.equal(s1, true);
+    // assert.equal(s2, true);
+  });
+
+  xit('returns true if there is a straight regardless of order', function () {
+    var s1 = checkHand.straightFlush({2: 1, 1: 1, 5: 1, 4: 1, 3: 1});
+    var s2 = checkHand.straightFlush({6: 1, 7: 1, 8: 1, 9: 1, 10: 1});
+
+    assert.equal(s1, true);
+    assert.equal(s2, true);
   });
 });
